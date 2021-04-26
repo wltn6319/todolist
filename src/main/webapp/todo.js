7cad1a77feddff8d37621249ed8b0d16e2487331
wltn6319 <wlwl6319@naver.com>
2021-04-26 19:53:08 +0900

    syntax modify

--- src/main/webapp/todo.js
+++ src/main/webapp/todo.js
@@ -16,10 +16,11 @@ function typeUpdate(type_list, type) {
 
 
 function clicked(id, type) {
-	var oReq  = new XMLHttpReuqest();
+	console.log("test");
+	var oReq  = new XMLHttpRequest();
 	var type_list = type.closest('li'); // type(input테그)에서 가장 가까운 li테그 부모를 찾음
 	type = type.closest('ul').getAttribute('id'); // type(input)에서 가장 가까운 ul테그의 id값을 찾음
-	oReq.addEventListenter("load", function() {
+	oReq.addEventListener("load", function() {
 		typeUpdate(type_list, type);
 	});
 	oReq.open("POST", "/todolist/todoType?id="+id+"&type="+type);
